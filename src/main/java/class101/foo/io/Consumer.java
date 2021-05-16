9877b9afac701d7930228d57301822576ab205a6
홍동수 <sug5806@naver.com>
2021-05-16 16:51:28 +0900

    feat: rabbitMQ 추가

---
+++ src/main/java/class101/foo/io/Consumer.java
@@ -0,0 +1,22 @@
+package class101.foo.io;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class Consumer {
+
+    private final ObjectMapper objectMapper;
+    private final PostRepository postRepository;
+
+    @RabbitListener(queues = "CREATE_POST_QUEUE")
+    public void handler(String message) throws JsonProcessingException {
+        Post post = objectMapper.readValue(message, Post.class);
+        postRepository.save(post);
+    }
+
+}
--- src/main/java/class101/foo/io/PostController.java
+++ src/main/java/class101/foo/io/PostController.java
@@ -1,25 +1,31 @@
 package class101.foo.io;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
 public class PostController {
 
     private static Integer PAGE_SIZE = 20;
 
-    @Autowired
-    PostRepository postRepository;
+    private final PostRepository postRepository;
+    private final Producer producer;
+    private final ObjectMapper objectMapper;
 
     // 1. 글을 작성한다.
     @PostMapping("/post")
-    public Post createPost(@RequestBody Post post) {
-        return postRepository.save(post);
+    public Post createPost(@RequestBody Post post) throws IOException {
+        String message = objectMapper.writeValueAsString(post);
+        producer.sendTo(message);
+        return post;
     }
 
     // 2-1. 글 목록을 조회한다.
@@ -30,7 +36,6 @@ public class PostController {
                 PageRequest.of(page - 1, PAGE_SIZE, Sort.by("id").descending())
         );
     }
-
 
     // 3. 글 번호로 조회
     @GetMapping("/post/{id}")

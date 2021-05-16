9fb2fc48122f303d389b93aa76c7a4a82bd9801c
홍동수 <sug5806@naver.com>
2021-05-16 14:28:31 +0900

    디폴트 경로 추가

---
+++ src/main/java/class101/foo/io/MainController.java
@@ -0,0 +1,13 @@
+package class101.foo.io;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    @GetMapping("/")
+    public String main() {
+        return "/";
+    }
+}

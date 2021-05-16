4a048f0cc31cc85cf9146df8b50bfb2cf7f10168
홍동수 <sug5806@naver.com>
2021-05-16 15:06:53 +0900

    feat: 글 내용으로 검색기능 추가

--- src/main/java/class101/foo/io/PostController.java
+++ src/main/java/class101/foo/io/PostController.java
@@ -6,6 +6,8 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 public class PostController {
 
@@ -35,7 +37,10 @@ public class PostController {
     public Post getPostById(@PathVariable("id") Long id) {
         return postRepository.findById(id).get();
     }
-    
-    // 4. 글 내용으로 검색 -> 해당 내용이 포함된 모든 글
 
+    // 4. 글 내용으로 검색 -> 해당 내용이 포함된 모든 글
+    @GetMapping("/search")
+    public List<Post> findPostByContent(@RequestParam String content) {
+        return postRepository.findByContentContains(content);
+    }
 }
